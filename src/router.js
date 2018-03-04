3671fc419ace93aa5eb17bf697424f52a0a4699d
cag2050 <cag2050@163.com>
2018-03-04 13:51:14 +0800

    m

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld
+        },
+        {
+            path: '/button',
+            name: '',
+            // 使用vue的[异步组件](https://cn.vuejs.org/v2/guide/components.html#异步组件)技术，可以实现按需加载
+            // 但是，这种情况下一个组件生成一个js文件
+            component: resolve => require(['./views/ButtonDemo'], resolve)
+        }
+    ]
+})
---
+++ src/views/ButtonDemo.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+#buttonDemo
+    mt-button(type='default' @click.native='handleClick') Default
+    mt-button(type='primary') primary
+    mt-button(type='danger') danger
+</template>
+
+<script>
+export default {
+    name: '',
+    data () {
+        return {
+        }
+    },
+    methods: {
+        handleClick () {
+            console.log('button clicked')
+            alert('button clicked')
+        }
+    }
+}
+</script>
+
+<style lang="stylus" type="text/stylus">
+
+</style>
